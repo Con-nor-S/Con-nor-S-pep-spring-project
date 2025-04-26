0d1b31b9e759714fc61abd8c37825154db24a717
Connor Strutt <aaaaaaaaaaaaaaassb@gmail.com>
2025-04-26 15:58:49 +0000

    Register and login

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,16 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.entity.Account;
+import com.example.service.AccountService;
+import com.example.service.MessageService;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +18,39 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+@Controller
+@ResponseBody
 public class SocialMediaController {
+
+    // Service objects
+    @Autowired
+    AccountService accountService;
+    @Autowired
+    MessageService messageService;
+
+    /**
+     * Attempts to register new account
+     * @param acc account to be registered
+     * @return BAD_REQUEST if account invalid, CONFLICT if account exists, OK and new account if created
+     */
+    @PostMapping("/register")
+    public ResponseEntity<Account> registerAccount(@RequestBody Account acc){
+        // Check validity of input
+        if(acc.getUsername().isEmpty() || acc.getPassword().length() < 4)
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        // Attempt creation
+        Account addedAccount = accountService.createAccount(acc);
+        // Create response
+        return addedAccount == null ? new ResponseEntity<>(HttpStatus.CONFLICT) : 
+            new ResponseEntity<>(addedAccount, HttpStatus.OK);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Account> loginAccount(@RequestBody Account acc){
+        Account loggedInAccount = accountService.findAccount(acc);
+        return loggedInAccount == null ? new ResponseEntity<>(HttpStatus.UNAUTHORIZED) :
+            new ResponseEntity<>(loggedInAccount, HttpStatus.OK);
+    }
+
 
 }
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,46 @@
 package com.example.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
+@Service
 public class AccountService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    /**
+     * Persists an account
+     * @param acc account to persist
+     * @return Account if valid account persisted, else null
+     */
+    public Account createAccount(Account acc){
+        // Check if account already exists
+        Optional<Account> existingAccount = accountRepository.findByUsername(acc.getUsername()); 
+        if(existingAccount.isPresent()){
+            return null;
+        }
+        // save new account
+        return accountRepository.save(acc);
+    }
+
+    /**
+     * Finds persisted account
+     * @param acc account to check for
+     * @return persisted account if one exists, else null
+     */
+    public Account findAccount(Account acc){
+        // Query account info
+        Optional<Account> existingAccount = 
+            accountRepository.findByUsernameAndPassword(acc.getUsername(), acc.getPassword());
+        // parse result
+        if(existingAccount.isPresent())
+            return existingAccount.get();
+        return null;
+    }
 }
